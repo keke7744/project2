cbdb604932fb8e91291663b7c0ee1c5962d458dd
keke7744 <41632496+keke7744@users.noreply.github.com>
2019-06-26 14:31:51 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+  
+  
+
+  
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{border:solid ,'1px': black,max:width,100:'vw',}}>
+
+    <h1 className={{title: 'red'}}>My title</h1>
+  
+    <br/>
+  
+  <img src="/meklaa imge.jpg" />
+  
+  <br/>
+  <br/>
+  <br/>
+  <img src="/mekla imge.jpg" />  
+</div>
+  );
+}
+  
+export default App;
